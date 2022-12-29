c062162407ebcf505e6bafa2800ae574263d35f9
Seungho <hsh47607@naver.com>
2022-12-29 14:17:57 +0900

    restaurant 클래스 변수 및 vo 클래스 변수 생성

--- src/main/java/com/ddingji/apirestaurant/domain/Restaurant.java
+++ src/main/java/com/ddingji/apirestaurant/domain/Restaurant.java
@@ -1,4 +1,23 @@
 package com.ddingji.apirestaurant.domain;
 
+import com.ddingji.apirestaurant.domain.vo.Place;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "restaurant")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Restaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "res_id")
+    private Long id;
+    private String name;
+    @Embedded
+    private Place place;
+
 }
--- src/main/java/com/ddingji/apirestaurant/domain/vo/Location.java
+++ src/main/java/com/ddingji/apirestaurant/domain/vo/Location.java
@@ -1,4 +1,13 @@
 package com.ddingji.apirestaurant.domain.vo;
 
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Embeddable
+@NoArgsConstructor
 public class Location {
+    private String x;
+    private String y;
 }
