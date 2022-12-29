e4b3354421efc205babdb60a9c926c679ca0fa5c
ansm6 <ansm6@192.168.219.107>
2022-12-29 15:36:55 +0900

    RestaurantController 비어있는 function 추가

--- src/main/java/com/ddingji/apirestaurant/conroller/RestaurantController.java
+++ src/main/java/com/ddingji/apirestaurant/conroller/RestaurantController.java
@@ -1,5 +1,18 @@
 package com.ddingji.apirestaurant.conroller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class RestaurantController {
 
+	@GetMapping
+	public void getRestaurantListByCategory() {
+		
+	}
+	
+	@GetMapping
+	public void selectRandomRestaurant() {
+		
+	}
 }
