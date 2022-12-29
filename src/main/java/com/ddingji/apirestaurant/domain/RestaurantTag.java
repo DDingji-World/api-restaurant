c17a7c651e21681b11090f9320b446bbf23021d1
Seungho <hsh47607@naver.com>
2022-12-29 14:38:45 +0900

    식당_태그 클래스 변수 선언

--- src/main/java/com/ddingji/apirestaurant/domain/RestaurantTag.java
+++ src/main/java/com/ddingji/apirestaurant/domain/RestaurantTag.java
@@ -1,4 +1,20 @@
 package com.ddingji.apirestaurant.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "restaurant_tag")
 public class RestaurantTag {
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "res_id")
+    private Restaurant restaurant;
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "tag_id")
+    private Tag tag;
 }
