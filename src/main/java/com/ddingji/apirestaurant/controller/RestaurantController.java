4081016bd288b63b48dfdeee79f059f55fa6329b
Seungho <hsh47607@naver.com>
2023-01-02 15:25:34 +0900

    api 변경 -> 하나의 api만 사용 / requestparam으로 분기

--- src/main/java/com/ddingji/apirestaurant/controller/RestaurantController.java
+++ src/main/java/com/ddingji/apirestaurant/controller/RestaurantController.java
@@ -1,17 +1,16 @@
 package com.ddingji.apirestaurant.controller;
 
-import com.ddingji.apirestaurant.controller.dto.RandomRestaurantResponse;
-import com.ddingji.apirestaurant.controller.dto.RestaurantListResponse;
+import com.ddingji.apirestaurant.controller.dto.RestaurantResponse;
+import com.ddingji.apirestaurant.domain.CategoryType;
 import com.ddingji.apirestaurant.service.RestaurantService;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,35 +18,10 @@ public class RestaurantController {
 
     private final RestaurantService restaurantService;
 
-    @GetMapping("/restaurantList")
-    public ResponseEntity<Map<String, Object>> getRestaurantListByCategory(@RequestParam("category") String name) {
-    	List<RestaurantListResponse> response = restaurantService.getRestaurantListByCategory(name);
-    	Map<String, Object> result = new HashMap<>();
-    	result.put("data", response);
-    	result.put("size", response.size());
-        return ResponseEntity.ok(result);
-    }
-    
-    @GetMapping("/restaurantList/all")
-    public ResponseEntity<Map<String, Object>> getRestaurantList() {
-    	List<RestaurantListResponse> response = restaurantService.getRestaurantList();
-    	Map<String, Object> result = new HashMap<>();
-    	result.put("data", response);
-    	result.put("size", response.size());
-        return ResponseEntity.ok(result);
-    }
-
-    @PostMapping("/restaurant")
-    public ResponseEntity<RandomRestaurantResponse> selectRandomRestaurantByCategory(
-            @RequestParam("category") String name) {
-        RandomRestaurantResponse response = restaurantService.selectRandomRestaurantByCategory(
-                name);
-        return ResponseEntity.ok(response);
-    }
-    @PostMapping("/restaurant/all")
-    public ResponseEntity<RandomRestaurantResponse> selectRandomRestaurant() {
-        RandomRestaurantResponse response = restaurantService.selectRandomRestaurant();
-        return ResponseEntity.ok(response);
+    @GetMapping("/restaurants")
+    @ResponseStatus(HttpStatus.OK)
+    public List<RestaurantResponse> getRestaurants(@RequestParam(name = "category", required = false) CategoryType categoryType) {
+        return restaurantService.getRestaurants(categoryType);
     }
 
 }
