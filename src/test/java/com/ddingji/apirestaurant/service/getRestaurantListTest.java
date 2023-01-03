3336af056bd39c04611c996e6aab471597ce1669
Seungho <hsh47607@naver.com>
2023-01-03 14:51:22 +0900

    testing get restaurant by id

--- src/test/java/com/ddingji/apirestaurant/service/getRestaurantListTest.java
+++ src/test/java/com/ddingji/apirestaurant/service/getRestaurantListTest.java
@@ -1,7 +1,11 @@
 package com.ddingji.apirestaurant.service;
 
+import com.ddingji.apirestaurant.controller.dto.RestaurantInfoResponse;
 import com.ddingji.apirestaurant.controller.dto.RestaurantResponse;
 import com.ddingji.apirestaurant.domain.CategoryType;
+import com.ddingji.apirestaurant.exception.RestaurantErrorCode;
+import com.ddingji.apirestaurant.exception.RestaurantException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,11 +27,25 @@ public class getRestaurantListTest {
 		restaurants.forEach(System.out::println);
 	}
 
-
 	@Test
 	@Transactional
 	void getRestaurantsWhenCategoryIsSpecific() {
 		List<RestaurantResponse> restaurants = restaurantService.getRestaurants(CategoryType.KOREAN);
 		restaurants.forEach(System.out::println);
+	}
+
+	@Test
+	@Transactional
+	void getRestaurantById() {
+		RestaurantInfoResponse response = restaurantService.getRestaurantById(1L);
+		Assertions.assertNotNull(response);
+	}
+
+	@Test
+	@Transactional
+	void getRestaurantByInvalidId() {
+		Assertions.assertEquals(RestaurantErrorCode.NOT_FOUND_RESTAURANT_DATA_BY_ID.getErrorMessage(),
+				Assertions.assertThrows(RestaurantException.class,
+					() -> restaurantService.getRestaurantById(100000000000000000L)).getErrorMessage());
 	}
 }
